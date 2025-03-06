28b56bb415ba611136db5e5b708f6296a48e618c
JacksonMessa <jacksonadrianomessa@gmail.com>
2025-03-06 15:44:55 -0300

    Porcentagem

---
+++ Distribuidora/Program.cs
@@ -0,0 +1,3 @@
+﻿double sp = 67836.43, rj = 36678.66, mg = 29229.88, es = 27165.48, outros = 19849.53;
+double total = sp + rj + mg + es + outros;
+Console.Write("Porcentagem dos estados dentro do valor total\nSP = " + (sp / total) * 100 + "%\nRJ = " + (rj / total) * 100 + "%\nMG = " + (mg / total) * 100 + "%\nES = " + (es / total) * 100 + "%\nOutros = " + (outros / total) * 100 + "%");
